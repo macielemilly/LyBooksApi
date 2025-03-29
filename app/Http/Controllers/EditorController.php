ead828fc34b102e7fcf20f302cafba36abbcf46c
Luanaa-Cordeiro <luana.cordeiro3@aluno.ce.gov.br>
2025-03-29 16:29:17 -0300

    Editora controller

---
+++ app/Http/Controllers/EditorController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Editor; 
+use Illuminate\Http\Request;
+
+class EditorController extends Controller
+{
+    
+    public readonly Editor $editor;
+
+    public function __construct(){
+        $this->editor = new Editor();
+    }
+
+    public function index()
+    {
+
+        $editoras = $this->editor->all();
+        return view('editor', ['editoras' => $editoras]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\EditorController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/home', [HomeController::class,'index'])->name('home');
+
+//->middleware(['auth', 'verified'])
+
+//Editora
+Route::get('/editoras', [EditorController::class,'index'])->name('editoras.index');
+Route::get('/editoras/create', [EditorController::class,'create'])->name('editoras.create');
+Route::post('/editoras', [EditorController::class,'store'])->name('editoras.store');
+Route::get('/editoras{editor}', [EditorController::class,'show'])->name('editoras.show');
+Route::get('/editoras/{editor}/edit', [EditorController::class,'edit'])->name('editoras.edit');
+Route::put('/editoras/{editor}', [EditorController::class,'update'])->name('editoras.update');
+Route::delete('/editoras/{editor}', [EditorController::class,'destroy'])->name('editoras.destroy');
 
 Route::get('/', function () {
     return view('welcome');
