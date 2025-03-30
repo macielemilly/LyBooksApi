62a2471783ccf9617be1f24d7e2ad45ebf9974d5
Luanaa-Cordeiro <luana.cordeiro3@aluno.ce.gov.br>
2025-03-30 20:41:30 -0300

    welcome e teste nav universal

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Document</title>
+</head>
+<body>
+@extends('nav')
+<div class="p-4 sm:ml-64">
+   <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+<h1>oi</h1>
+</div>
+</div>
+</body>
+</html>
