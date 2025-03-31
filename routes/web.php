6f6b0ca46c80fcacf74e7e97a74a9e4277b99f31
Luanaa-Cordeiro <luana.cordeiro3@aluno.ce.gov.br>
2025-03-31 18:22:39 -0300

    rotas generos

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\EditorController;
 use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\GeneroController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/home', [HomeController::class,'index'])->name('home');
@@ -19,6 +20,7 @@ Route::get('/editoras/{editor}/edit', [EditorController::class,'edit'])->name('e
 Route::put('/editoras/{editor}', [EditorController::class,'update'])->name('editoras.update');
 Route::delete('/editoras/{editor}', [EditorController::class,'destroy'])->name('editoras.destroy');
 
+//Linguagens
 Route::get('/languages', [LanguageController::class,'index'])->name('languages.index');
 Route::get('/languages/create', [LanguageController::class,'create'])->name('languages.create');
 Route::post('/languagens', [LanguageController::class,'store'])->name('languages.store');
@@ -26,6 +28,16 @@ Route::get('/languages{language}', [LanguageController::class,'show'])->name('la
 Route::get('/languages/{language}/edit', [languageController::class,'edit'])->name('languages.edit');
 Route::put('/languages/{language}', [languageController::class,'update'])->name('languages.update');
 Route::delete('/languages/{language}', [languageController::class,'destroy'])->name('languages.destroy');
+
+//Gêneros
+Route::get('/generos', [GeneroController::class,'index'])->name('generos.index');
+Route::get('/generos/create', [GeneroController::class,'create'])->name('generos.create');
+Route::post('/generos', [GeneroController::class,'store'])->name('generos.store');
+Route::get('/generos{genero}', [GeneroController::class,'show'])->name('generos.show');
+Route::get('/generos/{genero}/edit', [GeneroController::class,'edit'])->name('generos.edit');
+Route::put('/generos/{genero}', [GeneroController::class,'update'])->name('generos.update');
+Route::delete('/generos/{genero}', [GeneroController::class,'destroy'])->name('generos.destroy');
+
 
 Route::get('/', function () {
     return view('welcome');
