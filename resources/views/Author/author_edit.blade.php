3a01811db86b3992bc5dc92c03754ab9a8dee516
macielemilly <emillymaciel0123@gmail.com>
2025-04-07 22:11:11 -0300

    edit author

--- resources/views/Author/author_edit.blade.php
+++ resources/views/Author/author_edit.blade.php
@@ -21,13 +21,18 @@
         </div>
         <!-- Modal body -->
         <div class="p-4 md:p-5">
-        <form class="space-y-4" action="{{ route('authors.update', ['author' => $author->id])}}" method='post'>
+        <form class="space-y-4" action="{{ route('authors.update', ['author' => $authors->id])}}" method='post'>
                 @csrf
                     <div>
                         <label for="nome" class="block mb-2 text-sm font-medium text-gray-900">Nome do Autor<span class="asterisco">*</span></label>
                         <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
                           value="{{$authors->nome}}" type="text" id="nome" name="nome" placeholder="Nome"/>
                           <input type="hidden" name="_method" value="PUT">
+                    </div>
+                    <div>
+                    <label for="Descrição" class="block mb-2 text-sm font-medium text-gray-900">Descrição<span class="asterisco">*</span></label>
+                          <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
+                          value="{{$authors->descricao}}" type="text" id="descricao" name="descricao" placeholder="Descricao"/>
                     </div>
                     <button style="background-color:#D09953;" type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" id="criar">Editar
                     </button>
