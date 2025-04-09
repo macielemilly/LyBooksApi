f8b6f6c4d3307621ea18b07cae53dccf5d62824b
macielemilly <emillymaciel0123@gmail.com>
2025-04-08 21:56:44 -0300

    data em formato br

--- app/Models/Aluguel.php
+++ app/Models/Aluguel.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Aluguel extends Model
@@ -11,5 +11,15 @@ class Aluguel extends Model
     protected $fillable = [
         'nome','locatario','data_aluguel','data_devolucao'
     ];
+
+    public function getDataAluguelAttribute($value)
+    {
+        return Carbon::parse($value)->format('d/m/Y'); // Formato brasileiro: DD/MM/AAAA
+    }
+
+    public function getDataDevolucaoAttribute($value)
+    {
+        return Carbon::parse($value)->format('d/m/Y');
+    }
 }
 
