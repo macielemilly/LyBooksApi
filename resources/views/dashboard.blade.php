5705d4ea9bce0e5def2892d5d93a5dbd8c478ff8
Luanaa-Cordeiro <luana.cordeiro3@aluno.ce.gov.br>
2025-04-09 16:52:48 -0300

    mudança da dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,156 +20,108 @@
 </div>
 </div>
 
+<div class=" sm:ml-80">
+   <div class="p-3 rounded-lg mt-20">
+   <div class=" dash  relative overflow-x-auto  sm:rounded-lg">
 
-<div class="cartoes sm:ml-80">
-   <div  style="background-color:#013C3C; box-shadow: 0 4px 5px rgba(0, 0, 0, 0.267);;" class="p-3 rounded-lg mt-2">
-   <div class=" cartao relative overflow-x-auto shadow-md sm:rounded-lg">
-    <div class="p-5">
-        <a href="#">
-            <div class="itens_cartoes"> 
-            <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900">Livros</h5>
-            <img class="rounded-t-lg" src="{{ asset('assets/images/abra-o-livro.png') }}" alt="" />
+      <div class="graficos">
+            
+
+<div class=" grafic p-6 bg-white border border-gray-200 rounded-lg shadow-sm">
+    <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
+    </svg>
+    <a href="#">
+        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
+    <a href="#" class="inline-flex font-medium items-center text-blue-600 hover:underline">
+        See our guideline
+        <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
+        </svg>
+    </a>
+</div>
+
+      </div>
+
+      <div>
+        <div class="tabelas">
+            <div class="liv_gen">
+            <div style="height: 150px; margin: 0px 10px; width: 350px" class=" liv_ge max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+    <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
+    </svg>
+    <a href="#">
+        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Livros</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">:</p>
+</div>
+
+
+
+<div style="height: 150px; width: 350px;margin: 0px 10px; " class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+    <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
+    </svg>
+    <a href="#">
+        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Gêneros</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Quantidade: {{ $total_generos }}</p>
+</div>
+
             </div>
-        </a>
-        <p class="mb-3 font-normal text-gray-700">Quantidade: 0</p>
-        <div class="botao_home">
-        <a href="{{route('dashboard')}}">
-            <button class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg">Conferir
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-            </button>
-        </a>
+
+            <div class="aut_edit">
+
+
+            <div  style="height: 150px; width: 350px;margin: 10px 10px; "  class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+    <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
+    </svg>
+    <a href="#">
+        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Editoras</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Quantidade: {{ $total_editoras }}</p>
+</div>
+
+
+
+<div  style="height: 150px; width: 350px;margin: 10px 10px; " class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+    <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
+    </svg>
+    <a href="#">
+        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Autores</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Quant</p>
+</div>
+
+            </div>
+            
         </div>
+
+        <div class="info">
+        
+<div class=" p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+    <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z"/>
+    </svg>
+    <a href="#">
+        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">oi</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Quant</p>
+</div>
+
+        </div>
+      </div>
+
+
+
     </div>
 </div>
 </div>
-</div>
 
-<div class="cartoes sm:ml-80">
-<div  style="background-color:#013C3C; box-shadow: 0 4px 5px rgba(0, 0, 0, 0.267);;" class="p-3 rounded-lg mt-15">
-   <div class="cartao relative overflow-x-auto shadow-md sm:rounded-lg">
-    <div class="p-5">
-        <a href="#">
-            <div class="itens_cartoes"> 
-            <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900">Editoras</h5>
-            <img class="rounded-t-lg" src="{{ asset('assets/images/editora.png') }}" alt="" />
-            </div>
-        </a>
-        <p class="mb-3 font-normal text-gray-700">Quantidade: {{ $total_editoras }}</p>
-        <div class="botao_home">
-        <a href="{{route('editoras.index')}}">
-            <button class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg">Conferir
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-            </button>
-        </a>
-        </div>
-    </div>
-</div>
-</div>
-</div>
-
-<div class="cartoes sm:ml-80">
-<div  style="background-color:#013C3C; box-shadow: 0 4px 5px rgba(0, 0, 0, 0.267);;" class="p-3 rounded-lg mt-15">
-   <div class="cartao relative overflow-x-auto shadow-md sm:rounded-lg">
-    <div class="p-5">
-        <a href="#">
-            <div class="itens_cartoes"> 
-            <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900">Autores</h5>
-            <img class="rounded-t-lg" src="{{ asset('assets/images/autores.png') }}" alt="" />
-            </div>
-        </a>
-        <p class="mb-3 font-normal text-gray-700">Quantidade: 0</p>
-        <div class="botao_home">
-        <a href="{{route('dashboard')}}">
-            <button class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg">Conferir
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-            </button>
-        </a>
-        </div>
-    </div>
-</div>
-</div>
-</div>
-
-<div class="cartoes sm:ml-80">
-<div  style="background-color:#013C3C; box-shadow: 0 4px 5px rgba(0, 0, 0, 0.267);;" class="p-3 rounded-lg mt-15">
-   <div class="cartao relative overflow-x-auto shadow-md sm:rounded-lg">
-    <div class="p-5">
-        <a href="#">
-            <div class="itens_cartoes"> 
-            <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900">Linguagens</h5>
-            <img class="rounded-t-lg" src="{{ asset('assets/images/idiomas.png') }}" alt="" />
-            </div>
-        </a>
-        <p class="mb-3 font-normal text-gray-700">Quantidade: {{ $total_languages }}</p>
-        <div class="botao_home">
-        <a href="{{route('languages.index')}}">
-            <button class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg">Conferir
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-            </button>
-        </a>
-        </div>
-    </div>
-</div>
-</div>
-</div>
-
-<div class="cartoes sm:ml-80">
-<div  style="background-color:#013C3C; box-shadow: 0 4px 5px rgba(0, 0, 0, 0.267);;" class="p-3 rounded-lg mt-15">
-   <div class="cartao relative overflow-x-auto shadow-md sm:rounded-lg">
-    <div class="p-5">
-        <a href="#">
-            <div class="itens_cartoes"> 
-            <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900">Gêneros</h5>
-            <img class="rounded-t-lg" src="{{ asset('assets/images/fantasma.png') }}" alt="" />
-            </div>
-        </a>
-        <p class="mb-3 font-normal text-gray-700">Quantidade: {{ $total_generos }}</p>
-        <div class="botao_home">
-        <a href="{{route('generos.index')}}">
-            <button class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg">Conferir
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-            </button>
-        </a>
-        </div>
-    </div>
-</div>
-</div>
-</div>
-
-<div class="cartoes sm:ml-80">
-<div  style="background-color:#013C3C; box-shadow: 0 3px 5px rgba(0, 0, 0, 0.267);;" class="p-3 rounded-lg mt-15">
-   <div class="cartao relative overflow-x-auto shadow-md sm:rounded-lg">
-    <div class="p-5">
-        <a href="#">
-            <div class="itens_cartoes"> 
-            <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900">Alugados</h5>
-            <img class="rounded-t-lg" src="{{ asset('assets/images/abra-o-livro.png') }}" alt="" />
-            </div>
-        </a>
-        <p class="mb-3 font-normal text-gray-700">Quantidade: 0</p>
-        <div class="botao_home">
-        <a href="#">
-            <button class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white  rounded-lg">Conferir
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-            </button>
-        </a>
-        </div>
-    </div>
-</div>
-</div>
-</div>
 
 
 </body>
