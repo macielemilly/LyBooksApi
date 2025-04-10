34f7f56f31c8dfdbcdc5f76c0868cf7da427d673
Luanaa-Cordeiro <luana.cordeiro3@aluno.ce.gov.br>
2025-04-09 22:04:12 -0300

    modificações dashbord + total_authors + total_aluguel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,9 @@ use Illuminate\Http\Request;
 use App\Models\Editor;
 use App\Models\Genero;
 use App\Models\Language;
+use App\Models\Author;
+use App\Models\Aluguel;
+
 
 
 class HomeController extends Controller
@@ -14,11 +17,15 @@ class HomeController extends Controller
         $totalEditoras = Editor::count();
         $totalGeneros = Genero::count();
         $totalLanguages = Language::count();
+        $totalAuthors = Author::count();
+        $totalAluguel = Aluguel::count();
 
         return view('dashboard', [
             'total_editoras' => $totalEditoras,
             'total_generos' => $totalGeneros,
             'total_languages' => $totalLanguages,
+            'total_authors' =>  $totalAuthors,
+            'total_aluguel' =>   $totalAluguel
         ]);
         
     }
