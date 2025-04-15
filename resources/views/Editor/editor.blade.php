f03a8cf7909f2a8ec34d280619577f16a5ac2447
Luanaa-Cordeiro <luana.cordeiro3@aluno.ce.gov.br>
2025-04-15 20:12:46 -0300

    correção cards tabela

--- resources/views/Editor/editor.blade.php
+++ resources/views/Editor/editor.blade.php
@@ -95,18 +95,20 @@
     @else
 <div class="  mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
     @foreach($editoras as $editora)
-        <div style="background-color:#D09953;  box-shadow: 0 4px 5px rgba(0, 0, 0, 0.267);" class="p-3 rounded-lg">
-            <div style="background-color:white;" class="cartao relative overflow-x-auto shadow-md sm:rounded-lg border border-gray-400">
-                <div class="p-5">
+       
+    <div style="background-color:white;" class="cartao relative overflow-x-auto shadow-md sm:rounded-lg border border-gray-400">
+                <div class="">
                     <a href="#">
+                    <div style="background-color:#013C3C;" class="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200">
                         <div class="itens_cartoes">
-                            <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900">{{$editora->nome}}</h5>
+                            <h5 style="color:white" class="mb-1 text-2xl tracking-tight text-gray-900">{{$editora->nome}}</h5>
+                        </div>
                         </div>
                     </a>
-                    <p class="mb-3 font-normal text-gray-700">ID:  {{$editora->id}}</p>
-                    <div class="botao_tab flex gap-2">
+                    <p class="px-3 pt-3 mb-3 font-normal text-gray-700">ID: {{$editora->id}}</p>
+                    <div class="botao_tab mb-5 mr-5 flex gap-2">
                     <a href="{{ route('editoras.edit', ['editor' => $editora->id]) }}"><button class="editar" >Editar</button></a>
-                    <form class="inline" action="{{ route('editoras.destroy', ['editor' => $editora->id]) }}" method="POST" onsubmit="return confirm('Tem certeza que deseja excluir esta editora?');">
+                        <form class="inline" action="{{ route('editoras.destroy', ['editor' => $editora->id]) }}" method="POST" onsubmit="return confirm('Tem certeza que deseja excluir esta editora?');">
                                     @csrf
                                     <input type="hidden" name="_method" value="DELETE">
                                     <button type="submit" class="deletar text-red-600 hover:text-red-800">Excluir</button>
@@ -114,7 +116,6 @@
                     </div>
                 </div>
             </div>
-        </div>
     @endforeach
 </div>
 @endif
--- resources/views/Gêneros/genero.blade.php
+++ resources/views/Gêneros/genero.blade.php
@@ -92,28 +92,28 @@
     @else
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             @foreach($generos as $genero)
-                <div style="background-color:#D09953;  box-shadow: 0 4px 5px rgba(0, 0, 0, 0.267);" class="p-3 rounded-lg">
-                    <div style="background-color:white;" class="cartao relative overflow-x-auto shadow-md sm:rounded-lg border border-gray-400">
-                        <div class="p-5">
-                            <a href="#">
-                                <div class="itens_cartoes">
-                                    <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900">{{$genero->nome}}</h5>
-                                </div>
-                            </a>
-                            <p class="mb-3 font-normal text-gray-700">ID: {{$genero->id}}</p>
-                            <div class="botao_tab flex gap-2">
-                                <a href="{{ route('generos.edit', ['genero' => $genero->id]) }}">
-                                    <button style="background-color:green;" class="editar">Editar</button>
-                                </a>
-                                <form class="inline" action="{{ route('generos.destroy', ['genero' => $genero->id]) }}" method="POST" onsubmit="return confirm('Tem certeza que deseja excluir este gênero?');">
+            <div style="background-color:white;" class="cartao relative overflow-x-auto shadow-md sm:rounded-lg border border-gray-400">
+                <div class="">
+                    <a href="#">
+                    <div style="background-color:#013C3C;" class="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200">
+                        <div class="itens_cartoes">
+                            <h5 style="color:white" class="mb-1 text-2xl tracking-tight text-gray-900">{{$genero->nome}}</h5>
+                        </div>
+                        </div>
+                    </a>
+                    <p class="px-3 pt-3 mb-3 font-normal text-gray-700">ID: {{$genero->id}}</p>
+                    <div class="botao_tab mb-5 mr-5 flex gap-2">
+                    <a href="{{ route('generos.edit', ['genero' => $genero->id]) }}">
+                            <button style="background-color:green;" class="editar">Editar</button>
+                        </a>
+                        <form class="inline" action="{{ route('generos.destroy', ['genero' => $genero->id]) }}" method="POST" onsubmit="return confirm('Tem certeza que deseja excluir este gênero?');">
                                     @csrf
                                     <input type="hidden" name="_method" value="DELETE">
                                     <button type="submit" class="deletar text-red-600 hover:text-red-800">Excluir</button>
                                 </form>
-                            </div>
-                        </div>
                     </div>
                 </div>
+            </div>
             @endforeach
         </div>
     @endif
