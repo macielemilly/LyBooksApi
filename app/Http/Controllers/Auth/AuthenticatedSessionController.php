daa00a5a321e11d59e5ab597bd3bb5df75babfb1
macielemilly <emillymaciel0123@gmail.com>
2025-06-17 14:50:25 -0300

    aparecer nome no flutterflow

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -21,7 +21,9 @@ class AuthenticatedSessionController extends Controller
 
 
             return response()->json([
-                'token' => $token
+                'token' => $token,
+                'user' => $user
+                
             ]);
         }
 
